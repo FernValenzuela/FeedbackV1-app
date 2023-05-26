d08d55e8b864901cd696ef4a347d9a4e22f0f051
Fernando Valenzuela <99852078+FernValenzuela@users.noreply.github.com>
2023-05-25 20:25:39 -0500

    Changes

--- src/components/shared/Card.jsx
+++ src/components/shared/Card.jsx
@@ -1,5 +1,10 @@
-function Card() {
-  return <div>Card</div>
+function Card({ children, reverse }) {
+  return <div className={`card ${reverse && 'reverse'}`}>
+{children}</div>
+
+return (
+    <div className='card' style={}>{children}</div>
+)
 }
 
 export default Card
